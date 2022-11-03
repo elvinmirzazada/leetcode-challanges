6b7fc1a92fa28d78d3c0e3cf3e9388cca25c3eea
Elvin Mirzazada <mirzezade.elvin93@gmail.com>
2022-11-03 16:38:41 +0200

    Time: 1887 ms (39.70%), Space: 14.3 MB (27.99%) - LeetHub

---
+++ 0454-4sum-ii/0454-4sum-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def fourSumCount(self, nums1: List[int], nums2: List[int], nums3: List[int], nums4: List[int]) -> int:
+        cnt = 0
+        m = collections.defaultdict(int)
+        for a in nums1:
+            for b in nums2:
+                m[a + b] += 1
+        
+        print(m)
+        for c in nums3:
+            for d in nums4:
+                cnt += m[-(c + d)]
+                
+        return cnt
