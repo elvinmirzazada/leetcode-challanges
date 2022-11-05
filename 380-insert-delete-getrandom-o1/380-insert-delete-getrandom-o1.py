f10359831590101c80ceee1e83aa30f25e83d95f
Elvin Mirzazada <mirzezade.elvin93@gmail.com>
2022-11-05 12:07:52 +0200

    Time: 379 ms (99.50%), Space: 61.2 MB (85.33%) - LeetHub

--- 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
@@ -1,31 +1,40 @@
-class RandomizedSet:
-
+from random import choice
+class RandomizedSet():
     def __init__(self):
-        self.random_dict = {}
+        """
+        Initialize your data structure here.
+        """
+        self.dict = {}
+        self.list = []
 
+        
     def insert(self, val: int) -> bool:
-        if val in self.random_dict:
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        """
+        if val in self.dict:
             return False
-        else:
-            self.random_dict[val] = 1
-            return True
-
-    def remove(self, val: int) -> bool:
-        if val in self.random_dict:
-            del self.random_dict[val]
-            return True
-        else:
-            return False
+        self.dict[val] = len(self.list)
+        self.list.append(val)
+        return True
         
 
+    def remove(self, val: int) -> bool:
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        """
+        if val in self.dict:
+            # move the last element to the place idx of the element to delete
+            last_element, idx = self.list[-1], self.dict[val]
+            self.list[idx], self.dict[last_element] = last_element, idx
+            # delete the last element
+            self.list.pop()
+            del self.dict[val]
+            return True
+        return False
+
     def getRandom(self) -> int:
-        from random import randint
-        rand_int = randint(0, len(self.random_dict)-1)
-        return list(self.random_dict.keys())[rand_int]
-
-
-# Your RandomizedSet object will be instantiated and called as such:
-# obj = RandomizedSet()
-# param_1 = obj.insert(val)
-# param_2 = obj.remove(val)
-# param_3 = obj.getRandom()
+        """
+        Get a random element from the set.
+        """
+        return choice(self.list)
