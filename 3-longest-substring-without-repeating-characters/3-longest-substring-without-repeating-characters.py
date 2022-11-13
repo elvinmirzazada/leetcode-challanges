8c4d1be753e5d98e8e1c942aef00c88b809c1b69
Elvin Mirzazada <mirzezade.elvin93@gmail.com>
2022-11-13 13:36:20 +0200

    Time: 123 ms (65.69%), Space: 14.1 MB (13.94%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -8,12 +8,12 @@ class Solution:
             
             if ch not in sub_set:
                 sub_set.add(ch)
-                max_len = max(max_len, idx-start+1)
+                # max_len = max(max_len, idx-start+1)
+                max_len = max_len if max_len > (idx-start+1) else idx-start+1
             else:
                 while ch in sub_set:
                     sub_set.discard(s[start])
                     start += 1
                     
                 sub_set.add(ch)
-                # max_len = max(max_len, idx-start+1)
         return max_len
