50ae50d26316d9ac98478c324e4ad9ab544c397a
Elvin Mirzazada <mirzezade.elvin93@gmail.com>
2022-11-18 19:03:08 +0200

    Time: 146 ms (77.02%), Space: 16.9 MB (51.82%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.py
@@ -0,0 +1,38 @@
+from collections import defaultdict
+
+class Solution:
+    WHITE, GRAY, BLACK = 1, 2, 3
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj_map = defaultdict(list)
+        for course, must_course in prerequisites:
+            adj_map[must_course].append(course)
+            
+        # sorted_order = []
+        is_possible = True
+        
+        color = {k: Solution.WHITE for k in range(numCourses)}
+        
+        def dfs(node):
+            nonlocal is_possible
+            
+            if not is_possible:
+                return
+            
+            color[node] = Solution.GRAY
+            
+            if node in adj_map:
+                for n in adj_map[node]:    
+                    if color[n] == Solution.WHITE:
+                        dfs(n)
+                    elif color[n] == Solution.GRAY:
+                        is_possible = False
+                        
+            color[node] = Solution.BLACK
+            # sorted_order.append(node)
+            
+        for v in range(numCourses):
+            if color[v] == Solution.WHITE:
+                dfs(v)
+        
+        return is_possible
+        
