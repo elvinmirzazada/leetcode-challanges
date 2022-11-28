e5ca9d2e233956d54da380927291c5794d94a8f5
Elvin Mirzazada <mirzezade.elvin93@gmail.com>
2022-11-28 10:00:51 +0200

    Time: 97 ms (10.26%), Space: 14.2 MB (23.78%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,26 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        used = {i: False for i in range(len(nums))}
+        
+        
+        
+        
+        def backtracking(nums, permutation):
+
+            if len(permutation) == len(nums):
+                result.append(tuple(permutation))
+                return
+
+            for i in range(len(nums)):
+                if not used[i]:
+                    used[i] = True
+                    permutation.append(nums[i])
+                    backtracking(nums, permutation)
+                    permutation.pop()
+                    used[i] = False
+                    
+                    
+                    
+        backtracking(nums, [])
+        return result
